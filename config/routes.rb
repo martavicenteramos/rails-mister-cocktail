e07614f81a1ae7531f6add856e5e22502a2ddade
Marta Ramos <martavmr26297@gmail.com>
2019-11-15 12:01:39 +0000

    Added fonts to titles and created a root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   resources :cocktails do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: %i[new create destroy]
   end
 end
